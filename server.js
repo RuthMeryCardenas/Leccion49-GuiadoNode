568a5274f8c26bce8b8cd754477e4569b997cdee
RuthMeryCardenas <ruth.cardenas.perez@gmail.com>
2017-07-07 09:49:12 -0500

    Subiendo archivos

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var path    = require('path');
+var app     = express();
+
+app.set('port', (process.env.PORT || 8080) );
+app.use('/static', express.static(path.join(__dirname, 'node_modules')));
+app.use('/static', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res) => {
+  // res.send('Hello World!');
+  res.sendFile(__dirname + '/index.html');
+});
+
+app.listen(app.get('port'), () => {
+  console.log('Node app is running on port', app.get('port'));
+});
